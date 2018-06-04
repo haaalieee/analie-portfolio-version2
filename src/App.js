bad046a5812f7ef87d5025b712d8e53bb8fbc1ad
haaalieee <morenoanalie14@gmail.com>
2018-06-04 14:34:27 +0800

    Work Animation slide

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
-import WorkSection from './components/WorkSection'
+import WorkSectionCon from './components/WorkSectionCon'
 
 
 class App extends Component {
@@ -8,13 +8,7 @@ class App extends Component {
     return (
         <div>
           <Header/>
-          <WorkSection  
-                       trigger="trigger-work"   
-                       company="Schmuzter"
-                       job="Web Developer + Motion Content Designer"
-                       date="2017 - 2018"
-                       role="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at lectus sagittis tortor feugiat venenatis. Mauris vulputate, eros quis porttitor congue, magna dui blandit diam, quis volutpat nulla mi at felis. Nunc at pharetra ante, ac lacinia justo. Nunc facilisis dolor nec nisl aliquet, ac molestie turpis hendrerit. Proin fringilla maximus felis id tempus. Sed ornare quam at ligula pulvinar vestibulum. Pellentesque ac molestie ipsum. Mauris vel dui sit amet erat pharetra fringilla. Pellentesque eu tincidunt dui. Sed ac fermentum ipsum."
-                       />s
+          <WorkSectionCon/>
         </div>
     );
   }
--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -1,56 +1,37 @@
-import React from 'react';
-import ScrollMagic from "./ScrollMagic";
-import {TimelineMax, TweenMax, Linear} from 'gsap/TweenMax';
+import React from "react";
 
-class WorkSection extends React.Component{0
-    constructor(props){
-        super(props)
-    }
+class WorkSection extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    componentDidMount(){
-        var workTimeline = new TimelineMax();
-        var controller = new ScrollMagic.Controller();
-        var duration = 1;
-        var tween1 = TweenMax.from('.tweenwtext', duration, {clipPath:'inset(100% 0px 0%)', opacity: 0  , ease:Linear.easeInOut}, 4 );
-        var tween2 = TweenMax.from('.tweenwscreen', 0.5, { opacity: 0, ease:Linear.easeInOut}, -0.5 );
-        workTimeline.add(tween1).add(tween2);
-        var scene1 = new ScrollMagic.Scene({
-            triggerElement:'#trigger-work'
-        })
-        .addTo(controller);
-
-        scene1.setTween(workTimeline);
-    };
-
-    render(){
-        return(
-            <div>
-                <div className="work-fold" id="trigger-work">
-                    <div className="work-border">
-                       <div className="row w100 wfold" >
-                            <div className="col-md-6 pads">
-                                <div>
-                                    <h1 className="fs tweenwtext">{this.props.company}</h1>
-                                </div>
-                                    <div id="work-line" className="tweenwscreen"></div>
-                                    <div>
-                                        <h4 className="ss tweenwtext">{this.props.job}</h4>
-                                        <h6 className="ss tweenwtext">{this.props.date}</h6>
-                                        <br></br>
-                                        <h6 className="su tweenwtext">{this.props.role}</h6>
-                                    </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="tweenwscreen">
-                                    <img src="/images/AP-SEC-2-SCREEN.png" className="responsive headOne" alt="alie"/>
-                                </div>
-                            </div>
-                       </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="row w100 wfold">
+        <div className="col-md-6 pads">
+          <div>
+            <h1 className="fs tweenwtext">{this.props.company}</h1>
+          </div>
+          <div id="work-line" className="tweenwscreen" />
+          <div>
+            <h4 className="ss tweenwtext">{this.props.job}</h4>
+            <h6 className="ss tweenwtext">{this.props.date}</h6>
+            <br />
+            <h6 className="su tweenwtext">{this.props.role}</h6>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="tweenwscreen">
+            <img
+              src="/images/AP-SEC-2-SCREEN.png"
+              className="responsive headOne"
+              alt="alie"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default WorkSection;
