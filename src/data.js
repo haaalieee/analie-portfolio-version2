c75887a3decaee5a68e01e39c52334892ff4c398
haaalieee <morenoanalie14@gmail.com>
2018-06-16 19:43:16 +0800

    data images

--- src/data.js
+++ src/data.js
@@ -2,181 +2,181 @@ export const data = [
   {
     id: 1,
     name: "The Sound Cover 1",
-    image: "./images/1.jpg"
+    image: "/images/1.jpg"
   },
   {
     id: 2,
     name: "The Sound Cover 2",
-    image: "./images/2.jpg"
+    image: "/images/2.jpg"
   },
   {
     id: 3,
     name: "The Sound Cover 3",
-    image: "./images/3.jpg"
+    image: "/images/3.jpg"
   },
   {
     id: 4,
     name: "Archery Attack 1",
-    image: "./images/4.jpg"
+    image: "/images/4.jpg"
   },
   {
     id: 5,
     name: "Archery Attack 2",
-    image: "./images/5.jpg"
+    image: "/images/5.jpg"
   },
   {
     id: 6,
     name: "StudioM 1",
-    image: "./images/9.jpg"
+    image: "/images/9.jpg"
   },
   {
     id: 7,
     name: "Seiko 2",
-    image: "./images/7.jpg"
+    image: "/images/7.jpg"
   },
   {
     id: 8,
     name: "Seiko 3",
-    image: "./images/8.jpg"
+    image: "/images/8.jpg"
   },
   {
     id: 9,
     name: "Seiko 1",
-    image: "./images/6.jpg"
+    image: "/images/6.jpg"
   },
   {
     id: 10,
     name: "Coco 1",
-    image: "./images/10.jpg"
+    image: "/images/10.jpg"
   },
   {
     id: 11,
     name: "Coco 2",
-    image: "./images/11.jpg"
+    image: "/images/11.jpg"
   },
   {
     id: 12,
     name: "Coco 3",
-    image: "./images/12.jpg"
+    image: "/images/12.jpg"
   },
   {
     id: 13,
     name: "Coco 4",
-    image: "./images/13.jpg"
+    image: "/images/13.jpg"
   },
   {
     id: 14,
     name: "Coco 5",
-    image: "./images/14.jpg"
+    image: "/images/14.jpg"
   },
   {
     id: 15,
     name: "Coco 6",
-    image: "./images/15.jpg"
+    image: "/images/15.jpg"
   },
   {
     id: 16,
     name: "Coco 7",
-    image: "./images/16.jpg"
+    image: "/images/16.jpg"
   },
   {
     id: 17,
     name: "Coco 8",
-    image: "./images/17.jpg"
+    image: "/images/17.jpg"
   },
   {
     id: 18,
     name: "Mvp 1",
-    image: "./images/18.jpg"
+    image: "/images/18.jpg"
   },
   {
     id: 19,
     name: "Mvp 2",
-    image: "./images/19.jpg"
+    image: "/images/19.jpg"
   },
   {
     id: 20,
     name: "FDS1",
-    image: "./images/20.jpg"
+    image: "/images/20.jpg"
   },
   {
     id: 21,
     name: "FDS2",
-    image: "./images/21.jpg"
+    image: "/images/21.jpg"
   },
   {
     id: 22,
     name: "FDS3",
-    image: "./images/22.jpg"
+    image: "/images/22.jpg"
   },
   {
     id: 23,
     name: "FDS4",
-    image: "./images/23.jpg"
+    image: "/images/23.jpg"
   },
   {
     id: 24,
     name: "FDS5",
-    image: "./images/24.jpg"
+    image: "/images/24.jpg"
   },
   {
     id: 25,
     name: "FDS6",
-    image: "./images/25.jpg"
+    image: "/images/25.jpg"
   },
   {
     id: 26,
     name: "FDS7",
-    image: "./images/26.jpg"
+    image: "/images/26.jpg"
   },
   {
     id: 27,
     name: "FDS8",
-    image: "./images/27.jpg"
+    image: "/images/27.jpg"
   },
   {
     id: 28,
     name: "FDS9",
-    image: "./images/28.jpg"
+    image: "/images/28.jpg"
   },
   {
     id: 29,
     name: "FDS10",
-    image: "./images/29.jpg"
+    image: "/images/29.jpg"
   },
   {
     id: 30,
     name: "FDS10",
-    image: "./images/30.jpg"
+    image: "/images/30.jpg"
   },
   {
     id: 31,
     name: "FDS11",
-    image: "./images/31.jpg"
+    image: "/images/31.jpg"
   },
   {
     id: 32,
     name: "FDS12",
-    image: "./images/32.jpg"
+    image: "/images/32.jpg"
   },
   {
     id: 33,
     name: "FDS13",
-    image: "./images/33.jpg"
+    image: "/images/33.jpg"
   },
   {
     id: 34,
     name: "FDS14",
-    image: "./images/34.jpg"
+    image: "/images/34.jpg"
   },
   {
     id: 35,
     name: "FDS15",
-    image: "./images/35.jpg"
+    image: "/images/35.jpg"
   },
   {
     id: 36,
     name: "FDS16",
-    image: "./images/36.jpg"
+    image: "/images/36.jpg"
   }
 ];
